da81951094d92f2883ebda38f25c3f32933a9fbe
sstingo <tiffanychang1006@gmail.com>
2020-06-14 23:53:22 +0800

    activity_detail

--- afterclass_calendar/lib/activity_service/activity_http.dart
+++ afterclass_calendar/lib/activity_service/activity_http.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 
-import 'club_model.dart';
+import 'activity_model.dart';
 
 class ClubHttp {
-  final String url = "http://140.134.79.128:40130/club";
+  final String url = "http://140.134.79.128:40130/";
 
-  Future<List<ClubPost>> getClubPost() async {
+  Future<List<ActivityPost>> getClubPost() async {
     Response res = await get(url);
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
-      List<ClubPost> posts =
-          body.map((dynamic item) => ClubPost.fromJson(item)).toList();
+      List<ActivityPost> posts =
+          body.map((dynamic item) => ActivityPost.fromJson(item)).toList();
       return posts;
     } else {
       throw "Can't get posts.";
