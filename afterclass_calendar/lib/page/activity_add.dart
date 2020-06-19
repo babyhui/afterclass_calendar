d5a5b1eea5770c6d72294482c9b25176a9bfa5d5
Eria <c121680118@gmail.com>
2020-06-19 22:36:44 +0800

    輸入值

--- afterclass_calendar/lib/page/activity_add.dart
+++ afterclass_calendar/lib/page/activity_add.dart
@@ -2,23 +2,46 @@ import 'package:flutter/material.dart';
 
 class ActivityAdd extends StatelessWidget {
   @override
+   TextEditingController _unameController = TextEditingController();
+   TextEditingController _locationController = TextEditingController();
+   TextEditingController _moneyController = TextEditingController();
+   var a = '';
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('增加活動'),
+          
         ),
-        body: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text("名稱"),
-                TextField(
-                  cursorColor: Colors.blue,
-                  maxLength: 10,
-                ),
-              ],
-            )
-          ],
-        ));
+        body:Column(
+        children: <Widget>[
+          TextField(
+            autofocus: true,
+            controller: _unameController, //設定
+            
+            decoration: InputDecoration(
+              labelText: "活動名稱",
+              hintText: "輸入該次活動名稱",
+              prefixIcon: Icon(Icons.person),
+            ),
+          ),
+          TextField(
+            controller: _locationController,
+            decoration: InputDecoration(
+                labelText: "地點",
+                hintText: "您的活動地點",
+                prefixIcon: Icon(Icons.lock)),
+           
+          ),
+          TextField(
+            controller: _moneyController,
+            decoration: InputDecoration(
+                labelText: "入場費",
+                hintText: "活動費用",
+                prefixIcon: Icon(Icons.lock)),
+           
+          ),
+          RaisedButton(child: Text("送出"),onPressed: (){print(_unameController.text);},),
+        ],
+      ));
   }
 }
