11ee6145f549c3f418bc5118d0424d6704fec0d5
Eria <c121680118@gmail.com>
2020-06-20 01:37:17 +0800

    新增資料

--- afterclass_calendar/lib/page/activity_add.dart
+++ afterclass_calendar/lib/page/activity_add.dart
@@ -1,47 +1,63 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ActivityAdd extends StatelessWidget {
   @override
-   TextEditingController _unameController = TextEditingController();
-   TextEditingController _locationController = TextEditingController();
-   TextEditingController _moneyController = TextEditingController();
-   var a = '';
+  TextEditingController _unameController = TextEditingController();
+  TextEditingController _locationController = TextEditingController();
+  TextEditingController _moneyController = TextEditingController();
+  var a = '';
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('增加活動'),
-          
         ),
-        body:Column(
-        children: <Widget>[
-          TextField(
-            autofocus: true,
-            controller: _unameController, //設定
-            
-            decoration: InputDecoration(
-              labelText: "活動名稱",
-              hintText: "輸入該次活動名稱",
-              prefixIcon: Icon(Icons.person),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              autofocus: true,
+              controller: _unameController, //設定
+
+              decoration: InputDecoration(
+                labelText: "活動名稱",
+                hintText: "輸入該次活動名稱",
+                prefixIcon: Icon(Icons.person),
+              ),
             ),
-          ),
-          TextField(
-            controller: _locationController,
-            decoration: InputDecoration(
-                labelText: "地點",
-                hintText: "您的活動地點",
-                prefixIcon: Icon(Icons.lock)),
-           
-          ),
-          TextField(
-            controller: _moneyController,
-            decoration: InputDecoration(
-                labelText: "入場費",
-                hintText: "活動費用",
-                prefixIcon: Icon(Icons.lock)),
-           
-          ),
-          RaisedButton(child: Text("送出"),onPressed: (){print(_unameController.text);},),
-        ],
-      ));
+            TextField(
+              controller: _locationController,
+              decoration: InputDecoration(
+                  labelText: "地點",
+                  hintText: "您的活動地點",
+                  prefixIcon: Icon(Icons.lock)),
+            ),
+            TextField(
+              controller: _moneyController,
+              decoration: InputDecoration(
+                  labelText: "入場費",
+                  hintText: "活動費用",
+                  prefixIcon: Icon(Icons.lock)),
+            ),
+            RaisedButton(
+              child: Text("送出"),
+              onPressed: () async {
+                var url = 'http://140.134.79.128:40130/addActivity';
+                var search = {
+                  'name': _unameController.text,
+                  'location': _locationController.text,
+                  'money': _moneyController.text
+                };
+                var json = jsonEncode(search);
+                var response = await http.post(url,
+                    headers: {
+                      'Content-Type': 'application/json; charset=UTF-8'
+                    },
+                    body: json);
+              },
+            ),
+          ],
+        ));
   }
 }
